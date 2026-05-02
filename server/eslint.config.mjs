2f2bfc510072335d7f28bb3b8f5af9647c3215f2
Dejon Gill <dejonjgill@gmail.com>
2026-05-02 16:52:54 -0400

    chore(deps): add security linting
    Add eslint plugins n, security and unicorn. Shift left

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,25 +1,38 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import globals from 'globals';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import path from "path";
+import { fileURLToPath } from "url";
+import n from "eslint-plugin-n";
+import security from "eslint-plugin-security";
+import unicorn from "eslint-plugin-unicorn";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config([
   js.configs.recommended,
   tseslint.configs.recommended,
+  n.configs["flat/recommended"],
+  security.configs.recommended,
+  unicorn.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
       },
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
     },
   },
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     rules: {
-      '@typescript-eslint/no-require-imports': 'off',
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
   {
-    files: ['tests/**'],
+    files: ["tests/**"],
     languageOptions: {
       globals: {
         ...globals.jest,
@@ -27,6 +40,6 @@ export default tseslint.config([
     },
   },
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ["dist/**", "node_modules/**"],
   },
 ]);
